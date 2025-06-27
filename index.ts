79394ce0caecdb1ffb0db4b6f08873f02bc3d664
Cleo-s <cleo.mics@gmail.com>
2025-06-27 18:31:21 +0200

    upd: 3 out of 10 tasks for practicing regex - done

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-const text = 'Hello, World - .1234+ _';
+const text = 'Hello, hello, World, world- .1234+ _';
 
-// const regexp = /[0-9]+[a-z]*/ig;
-// const regexp = /[^a-z\s,-.+_]+/ig;
-const regexp = /\w+/g;
+const solution1 = /\d+/g;
+const solution2 = /[^A-Z0-9\s,.-_+]+/g;
+const solution3 = /[^a-zA-Z0-9\s]+/g;
 
-console.log(text.match(regexp));
+console.log(text.match(solution2));
