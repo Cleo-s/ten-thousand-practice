4a8a383d6ff9d93150f665188162222507ecdeca
Cleo-s <cleo.mics@gmail.com>
2025-07-07 16:01:21 +0200

    upd: basic practice of regex based on exam tasks

--- index1.ts
+++
@@ -1,5 +0,0 @@
-const text = 'Hello Mike. Here is my phone number +38 (098) 330-00-03, my wife phone number 068-339-09-09 and 920462 my sun number 0683390791. Please use same country code +38 to have opportunity to call me';
-
-const solution = /(?:\+)\d+/g;
-
-console.log(text.match(solution))
