1e5987985a636a6a2c98a5dbd743fa9f0de6458b
Cleo-s <cleo.mics@gmail.com>
2025-07-08 18:19:12 +0200

    upd: found a better, faster, shorter solution for proper regex iterations

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ const text = 'Hello Mike. Here is my phone number +38 (098) 330-00-03, my wife p
 //const text = 'Hello Mike. Here is my email is viktor.chukhlov1991@submail.tenthousand.io, @gmail my wife email 1991wife@story-telling.com and my test@gmail.com sun email 05w05v30eyje2v1.v@as.check24.eu. Tag bot with @service.io, don`t make a mistake with service@io';
 
 const solution = /(\+?\d+\s?\(?\-?\d+\)?)\s?(\d+\-?\d+\-?)\d+/g;
+const shorterSolution = /(\+?\d+\s?\(?\d+\)?\s?\d+)(\-?\d+)+/g;
 
-console.log(text.match(solution));
+console.log(text.match(shorterSolution));
