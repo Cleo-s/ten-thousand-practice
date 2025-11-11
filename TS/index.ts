8d0e13d70c22cf852478665cc5fc171c9bc52ecb
Cleo-s <cleo.mics@gmail.com>
2025-11-11 23:49:01 +0100

    upd: tasks for TS, CSS day 2 - done

--- TS/index.ts
+++ TS/index.ts
@@ -1,17 +1,50 @@
-const points: number = 100;
+//-----------------------------------------------------TASK 1-----------------------------------------------------//
 
-function CalculatePoints(bonusPoints: number): number {
-    return points + bonusPoints;
+type color = ('red' | 'yellow' | 'blue');
+
+//const currentLight: color = 'blue';
+
+//-----------------------------------------------------TASK 2-----------------------------------------------------//
+
+function nextLight(currentLight: color): color {
+    switch (currentLight) {
+        case 'blue': return 'yellow';
+        case 'yellow': return 'red';
+        case 'red': return 'blue';
+        default: return 'blue';
+    }
 }
 
-console.log(CalculatePoints(50));
+console.log(nextLight("blue"));
 
-type User = {name: string, age: number}
+//-----------------------------------------------------TASK 3-----------------------------------------------------//
 
-function showUser(user: User): User {
-    return user;
+enum Language {
+    JS,
+    TS,
+    CSS
 }
 
-const user: User = {name: 'Misha', age: 21};
+// function LogLang(lang: Language): string {
+//     switch (lang) {
+//         case Language.TS: return 'You chose TS';
+//         case Language.CSS: return 'You chose CSS';
+//         case Language.JS: return 'You chose JS';
+//         default: 'Choose Your Language';
+//     }
+// }
 
-console.log(showUser(user));
+// console.log(LogLang(Language.TS));
+
+function LogLang(lang: Language): string {
+    if (lang === Language.TS)
+        return 'You chose TS!';
+    else if (lang === Language.CSS) 
+        return 'You chose CSS';
+    else if (lang === Language.JS)
+        return 'Your default language is JS';
+    else 
+        return '1';
+}
+
+console.log(LogLang(Language.JS));
