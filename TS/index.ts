279359f7373cd7a0ca1f9540cbd98636e8d3bf6b
Cleo-s <cleo.mics@gmail.com>
2025-11-14 22:42:29 +0100

    upd: TS tassk day 4 - done

--- TS/index.ts
+++ TS/index.ts
@@ -1,63 +1,44 @@
-type User1 = {
-    name: string,
-    age: number
-};
+//-----------------------------------------------------TASK 1-----------------------------------------------------//
 
-const user: User1 = {
-    name: 'Misha',
-    age: 21
+function printId(id: string | number): number | string {
+    return typeof(id) === 'string' ? id.toUpperCase() : id;
 }
 
-function printUser(user: User1): string {
-    return user.name;
+console.log(printId(1));
+
+//-----------------------------------------------------TASK 2-----------------------------------------------------//
+
+type Circle = {
+    type: 'circle';
+    radius: number;
 }
 
-console.log(printUser(user))
-
-type calculator = {
-    add: (a: number, b: number) => number;
-    substract: (a: number, b: number) => number; 
+type Square = {
+    type: 'square',
+    size: number
 }
 
-const calc: calculator = {
-    add(a, b) {
-        return a + b;
-    },
-    substract(a, b) {
-        return a - b;
-    }
+type UnionType = Circle | Square;
+
+function findP(obj: UnionType): number {
+    return obj.type === 'circle' ? Math.PI * obj.radius ** 2 : obj.size * obj.size; 
 }
 
-console.log(calc.add(10, 3));
-console.log(calc.substract(7, 4));
+const shape: UnionType = {
+    type: 'square',
+    size: 2,
+}
+
+console.log(findP(shape));
+
+//-----------------------------------------------------TASK 3-----------------------------------------------------//
+
 
 type User = {
     name: string;
     age: number;
-    sayHi: () => string;
-    setName: (newName: string) => void;
-    addAge: () => void;
 }
 
-const newUser: User = {
-    name: 'Misha',
-    age: 21,
-    sayHi() {
-        return `Hi, ${this.name}`;
-    },
-    setName(newName) {
-        return this.name = newName;
-    },
-    addAge() {
-        return this.age + 1;
-    }
+function isUser(x: any): x is User {
+    return x && typeof x.name === 'string' && typeof x.age === 'number';
 }
-
-console.log(newUser.age)
-console.log(newUser.addAge())
-console.log(newUser.age)
-console.log(newUser.sayHi())
-console.log(newUser.name)
-console.log(newUser.setName('Vlad'))
-console.log(newUser.name)
-console.log(newUser.sayHi())
