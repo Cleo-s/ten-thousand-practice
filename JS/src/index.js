ecf16b65de0a7f0f0d2b82e989fb38e58a5940ba
Cleo-s <cleo.mics@gmail.com>
2025-11-26 22:09:49 +0100

    upd: day 16 tasks for js 2/3 - done + change of html and css

--- JS/src/index.js
+++ JS/src/index.js
@@ -1,5 +1,8 @@
 const button = document.querySelector('#click-button');
 const paragraph = document.querySelector('p');
+const input = document.querySelector('#message');
+const span = document.querySelector('#counter')
+
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
@@ -8,3 +11,13 @@ button.addEventListener('click', (e) => {
     paragraph.style.display = 'none' :
     paragraph.style.display = 'block'
 });
+
+input.addEventListener('input', (e) => {
+    e.preventDefault();
+
+    const inputLength = input.value.length;
+    span.textContent = `Length: ${inputLength}`;
+
+    inputLength >= 20 ? span.style.color = 'red' : span.style.color = 'black';
+})
+
