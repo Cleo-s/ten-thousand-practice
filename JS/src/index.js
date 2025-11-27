ab0b76e64701cfdb40bd228b1a744ab9744128ee
Cleo-s <cleo.mics@gmail.com>
2025-11-27 20:29:59 +0100

    upd: day 17 tasks for js 3/3 - done + change of html and css

--- JS/src/index.js
+++ JS/src/index.js
@@ -1,23 +1,60 @@
-const button = document.querySelector('#click-button');
-const paragraph = document.querySelector('p');
-const input = document.querySelector('#message');
-const span = document.querySelector('#counter')
+const buttonOne = document.getElementById('click-button');
+const paragraphOne = document.getElementById('p');
+const inputOne = document.getElementById('message');
+const spanOne = document.getElementById('counter');
+const inputTwo = document.getElementById('todo-input');
+const buttonTwo = document.getElementById('todo-button');
+const ulOne = document.getElementById('todo-list');
 
 
-button.addEventListener('click', (e) => {
+buttonOne.addEventListener('click', (e) => {
     e.preventDefault();
     
-    paragraph.style.display === 'block' ? 
-    paragraph.style.display = 'none' :
-    paragraph.style.display = 'block'
+    paragraphOne.style.display === 'block' ? 
+    paragraphOne.style.display = 'none' :
+    paragraphOne.style.display = 'block'
 });
 
-input.addEventListener('input', (e) => {
+inputOne.addEventListener('input', (e) => {
     e.preventDefault();
 
-    const inputLength = input.value.length;
-    span.textContent = `Length: ${inputLength}`;
+    const inputLength = inputOne.value.length;
+    spanOne.textContent = `Length: ${inputLength}`;
 
-    inputLength >= 20 ? span.style.color = 'red' : span.style.color = 'black';
+    inputLength >= 20 ? spanOne.style.color = 'red' : spanOne.style.color = 'black';
 })
 
+let inputTwoValue = '';
+
+inputTwo.addEventListener('input', (e) => {
+    e.preventDefault();
+
+    inputTwoValue = e.target.value;
+});
+
+
+buttonTwo.addEventListener('click', (e) => {
+e.preventDefault();
+
+    if (inputTwoValue) {
+        const li = document.createElement('li');
+        
+        li.textContent = inputTwoValue;
+
+        ulOne.appendChild(li);
+
+    };
+
+    inputTwo.value = '';
+    
+    const liItems = ulOne.querySelectorAll('li');
+    
+    liItems.forEach((li, index) => {
+        li.id = `item-${index+1}`;
+
+        li.onclick = (e) => {
+            const currentLi = e.target;
+            currentLi.style.textDecoration = 'line-through';
+        }
+    });
+});
