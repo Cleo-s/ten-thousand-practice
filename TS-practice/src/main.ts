6c934256c76ff8f7f8346a75ee347dd1ccd3ae1b
Cleo-s <cleo.mics@gmail.com>
2025-11-28 21:13:30 +0100

    upd: dau 18 new Vite env, tasks for TS 2/3 - done

--- TS-practice/src/main.ts
+++ TS-practice/src/main.ts
@@ -1,19 +1,46 @@
-import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const incBtn = document.getElementById('inc-btn') as HTMLButtonElement;
+const decBtn = document.getElementById('dec-btn') as HTMLButtonElement;
+const totalSpan = document.getElementById('amount-otp') as HTMLSpanElement;
+const userNameInput = document.getElementById('user-name-input') as HTMLInputElement;
+const greetUserBtn = document.getElementById('greet-btn') as HTMLButtonElement;
+const greetSpan = document.getElementById('greet-span') as HTMLSpanElement;
+
+let iniValue: number = 0;
+
+if (!incBtn || !decBtn || !totalSpan) 
+  console.error('Error');
+else 
+  totalSpan.textContent = `Count is: ${iniValue}`;
+
+incBtn.addEventListener('click', () => {
+  iniValue += 1;
+  totalSpan.textContent = `Count is: ${iniValue}`;
+});
+
+decBtn.addEventListener('click', () => {
+  iniValue -= 1;
+  totalSpan.textContent = `Count is: ${iniValue}`;
+});
+
+if (!userNameInput || !greetSpan || !greetUserBtn)  
+  console.log('Error');
+
+let userName: string = '';
+
+userNameInput.addEventListener('input', (e: Event) => {
+  
+  const target = e.target as HTMLInputElement | null;
+  if (!target) return;
+
+  userName = target.value;
+});
+
+greetUserBtn.addEventListener('click', () => {
+  if (!userName)
+    greetSpan.textContent = 'Please enter your Username';
+  else {
+    const trimmedUserName: string = userName.trim();
+    greetSpan.textContent = `Hi there, ${trimmedUserName}`;
+  } 
+})
