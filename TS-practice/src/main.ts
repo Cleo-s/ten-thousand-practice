ffa652e95bc9aebc126f3082c6bd9ce108fb4673
Cleo-s <cleo.mics@gmail.com>
2025-12-02 21:01:10 +0100

    upd: day 22 TS practice in progress + minor HTML & CSS changes

--- TS-practice/src/main.ts
+++ TS-practice/src/main.ts
@@ -1,90 +1,24 @@
+const taskInput = document.getElementById('task-input') as HTMLInputElement;
+const addTaskBtn = document.getElementById('add-task-btn') as HTMLButtonElement;
+const tasksList = document.getElementById('tasks-list') as HTMLUListElement;
 
-const incBtn = document.getElementById('inc-btn') as HTMLButtonElement;
-const decBtn = document.getElementById('dec-btn') as HTMLButtonElement;
-const totalSpan = document.getElementById('amount-otp') as HTMLSpanElement;
-const userNameInput = document.getElementById('user-name-input') as HTMLInputElement;
-const greetUserBtn = document.getElementById('greet-btn') as HTMLButtonElement;
-const greetSpan = document.getElementById('greet-span') as HTMLSpanElement;
-const colorDiv = document.getElementById('block') as HTMLDivElement;
-const btnBlock = document.getElementById('btn-collection') as HTMLDivElement;
+let currentTaskName: string = '';
 
-let iniValue: number = 0;
-
-if (!incBtn || !decBtn || !totalSpan) 
-  console.error('Error');
-else 
-  totalSpan.textContent = `Count is: ${iniValue}`;
-
-incBtn.addEventListener('click', () => {
-  iniValue += 1;
-  totalSpan.textContent = `Count is: ${iniValue}`;
+taskInput.addEventListener('input', () => {
+  currentTaskName = taskInput.value;
 });
 
-decBtn.addEventListener('click', () => {
-  iniValue -= 1;
-  totalSpan.textContent = `Count is: ${iniValue}`;
-});
+addTaskBtn.addEventListener('click', (e) => {
+  e.preventDefault();
 
-/*------------------------------------------------------------------TASK 1------------------------------------------------------------*/
+  if (!currentTaskName) return;
 
-if (!userNameInput || !greetSpan || !greetUserBtn)  
-  console.log('Error');
+  if (currentTaskName.length != 0) {
+    currentTaskName.trim()
 
-let userName: string = '';
+    const li: HTMLLIElement = document.createElement('li');
+    li.textContent = currentTaskName;
 
-userNameInput.addEventListener('input', (e: Event) => {
-  
-  const target = e.target as HTMLInputElement | null;
-  if (!target) return;
-
-  userName = target.value;
-});
-
-greetUserBtn.addEventListener('click', () => {
-  if (!userName)
-    greetSpan.textContent = 'Please enter your Username';
-  else {
-    const trimmedUserName: string = userName.trim();
-    greetSpan.textContent = `Hi there, ${trimmedUserName}`;
-  } 
-})
-
-/*------------------------------------------------------------------TASK 2------------------------------------------------------------*/
-
-type BtnId = 'btn-1' | 'btn-2' | 'btn-3';
-type BtnData = {
-  id: BtnId;
-  color: string;
-} 
-type DataSet = {
-  [key in BtnId]: BtnData;
-}
-
-const btnDataSet: DataSet = {
-  "btn-1": {id: 'btn-1', color: 'red'},
-  "btn-2": {id: 'btn-2', color: 'yellow'},
-  "btn-3": {id: 'btn-3', color: 'blue'}, 
-}
-
-
-function changeColor(colorDiv: HTMLDivElement, btnBlock: HTMLDivElement, btnDataSet: DataSet) {
-  if (btnBlock) {
-    const buttonsA = btnBlock.querySelectorAll<HTMLButtonElement>('button');
-    buttonsA.forEach((btn) => {
-      if (!(btn.id in btnDataSet)) return;
-      const config = btnDataSet[btn.id as keyof DataSet];
-
-      btn.style.backgroundColor = config.color
-
-      if (!btn) return;
-      
-      btn.textContent = `${config.color}`;
-
-      btn.addEventListener('click', () => {
-        colorDiv.style.backgroundColor = config.color;
-      })
-    })
+    tasksList.appendChild(li);
   }
-}
-
-changeColor(colorDiv, btnBlock, btnDataSet);
+})
